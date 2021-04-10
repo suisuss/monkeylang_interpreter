a85e2802c34db5b12dd51fb0ad7a40d4db66ee4a
Jacob Sussmilch <jacobsussmilch187@gmail.com>
2021-04-10 22:09:31 +1000

    Initial commit. Session one. Up to 1.4 (page 22)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/suisuss/monkey-interpreter
+
+go 1.16
---
+++ token/token.go
@@ -0,0 +1,48 @@
+package token
+
+import (
+	// "fmt"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+
+// TokenTypes
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdentifier(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
